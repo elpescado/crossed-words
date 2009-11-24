0b286073e2e59acc1e975ac0ab9db1652b7b4a5d
przemyslaw.sitek <przemyslaw.sitek@608399dc-c5a5-11de-afa3-455a29e9879b>
2009-11-24 22:46:37 +0000

    Windows files

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -26,10 +26,14 @@ int main (int argc, char *argv[])
 	gtk_init (&argc, &argv);
 	srand (time (NULL));
 
+	#ifdef __WIN32
+	gtk_rc_parse ("gtkrc");
+	#endif
+
 	main_window = scx_main_window_new ();
 	gtk_widget_show (main_window);
 	g_signal_connect (G_OBJECT (main_window), "delete-event", G_CALLBACK (gtk_main_quit), NULL);
-	
+
 	gtk_main ();
 
 	return EXIT_SUCCESS;
